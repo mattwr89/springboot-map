f064c296d52a6fc33943a1583f1ce33c9624231c
mattwr89 <maryniak.mat@gmail.com>
2020-04-15 13:33:45 +0200

    initial commit

---
+++ src/main/java/pl/mattwr89/springbootmap/SpringbootMapApplication.java
@@ -0,0 +1,13 @@
+package pl.mattwr89.springbootmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mattwr89/springbootmap/SpringbootMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mattwr89.springbootmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
